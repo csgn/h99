55c9972ef2c31a4683365572cb9d56b0b1d4b7b4
csgn <dev.csgn@gmail.com>
2025-02-21 22:13:30 +0300

    solve first 15

--- src/H99.hs
+++ src/H99.hs
@@ -1,7 +1,141 @@
 module H99 where
 
+-- https://ninetynine.haskell.chungyc.org/Problems.html#t:NestedList
+data NestedList a
+  = Elem a
+  | List [NestedList a]
+  deriving (Eq, Show)
+
+-- https://ninetynine.haskell.chungyc.org/Problems.html#t:Encoding
+data Encoding a
+  = Single a
+  | Multiple Int a
+  deriving (Eq, Show)
+
 -- Problem 1
 myLast :: [a] -> Maybe a
 myLast [] = Nothing
 myLast [x] = Just x
-myLast (_:xs) = myLast xs
+myLast (_ : xs) = myLast xs
+
+-- Problem 2
+myButLast :: [a] -> Maybe a
+myButLast [] = Nothing
+myButLast [_] = Nothing
+myButLast [x, _] = Just x
+myButLast (_ : xs) = myButLast xs
+
+-- Problem 3
+elementAt :: [a] -> Int -> Maybe a
+elementAt xs index = elementAt' xs index 1
+  where
+    elementAt' [] _ _ = Nothing
+    elementAt' (x : xs') i c
+      | i == c = Just x
+      | otherwise = elementAt' xs' i (c + 1)
+
+-- Problem 4
+myLength :: [a] -> Int
+myLength xs = myLength' xs 0
+  where
+    myLength' [] c = c
+    myLength' (_ : xs') c = myLength' xs' (c + 1)
+
+-- Problem 5
+myReverse :: [a] -> [a]
+myReverse xs = myReverse' xs []
+  where
+    myReverse' [] acc = acc
+    myReverse' (x : xs') acc = myReverse' xs' (x : acc)
+
+-- or
+-- myReverse = foldr (\x -> (++ [x])) []
+
+-- Problem 6
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = myReverse xs == xs
+
+-- Problem 7
+flatten :: NestedList a -> [a]
+flatten a = flatten' a []
+  where
+    flatten' a' acc = case a' of
+      Elem x -> x : acc
+      List [] -> acc
+      List [x] -> flatten' x acc
+      List (x : xs) -> flatten' x acc ++ flatten' (List xs) acc
+
+-- Problem 8
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress (x : xs) = compress' xs x [x]
+  where
+    compress' [] _ acc = acc
+    compress' (x' : xs') lstel acc
+      | x' == lstel = compress' xs' lstel acc
+      | otherwise = compress' xs' x' (acc ++ [x'])
+
+-- Problem 9
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x : xs) = pack' xs x [x] []
+  where
+    pack' [] _ acc acc' = acc' ++ [acc]
+    pack' (x' : xs') lstel acc acc'
+      | x' == lstel = pack' xs' lstel (acc ++ [x']) acc'
+      | otherwise = pack' xs' x' [x'] (acc' ++ [acc])
+
+-- Problem 10
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode a = encode' (pack a) []
+  where
+    encode' [] acc = acc
+    encode' (x : xs) acc = encode' xs (acc ++ [(length x, head x)])
+
+-- Problem 11
+encodeModified :: (Eq a) => [a] -> [Encoding a]
+encodeModified a = encodeModified' (encode a) []
+  where
+    encodeModified' [] acc = acc
+    encodeModified' ((x, y) : xs) acc
+      | x == 1 = encodeModified' xs (acc ++ [Single y])
+      | otherwise = encodeModified' xs (acc ++ [Multiple x y])
+
+-- Problem 12
+decodeModified :: [Encoding a] -> [a]
+decodeModified ea = decodeModified' ea []
+  where
+    decodeModified' [] acc = acc
+    decodeModified' (x : xs) acc = case x of
+      Single v -> decodeModified' xs (acc ++ [v])
+      Multiple n v -> decodeModified' xs (acc ++ replicate n v)
+
+-- Problem 13
+encodeDirect :: (Eq a) => [a] -> [Encoding a]
+encodeDirect [] = []
+encodeDirect (x : xs) = encodeDirect' xs x 1 []
+  where
+    encodeDirect' [] lstel count acc
+      | count == 1 = acc ++ [Single lstel]
+      | count > 1 = acc ++ [Multiple count lstel]
+      | otherwise = acc
+    encodeDirect' (x' : xs') lstel count acc
+      | x' /= lstel =
+          if count == 1
+            then encodeDirect' xs' x' 1 (acc ++ [Single lstel])
+            else encodeDirect' xs' x' 1 (acc ++ [Multiple count lstel])
+      | otherwise = encodeDirect' xs' lstel (count + 1) acc
+
+-- Problem 14
+dupli :: [a] -> [a]
+dupli as = dupli' as []
+  where
+    dupli' [] acc = acc
+    dupli' (x : xs) acc = dupli' xs (acc ++ [x, x])
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli as n = repli' as n []
+  where
+    repli' [] _ acc = acc
+    repli' (x : xs) n' acc = repli' xs n' (acc ++ replicate n' x)
--- test/H99-test.hs
+++ test/H99-test.hs
@@ -1,24 +1,120 @@
 module Main (main) where
 
-import Test.HUnit
 import H99
+import Test.HUnit
 
 -- Test cases
 -- Problem 1
 testMyLast :: Test
 testMyLast = TestCase $ do
-    assertEqual "should be Just 4" (Just (4::Int)) (myLast [1, 2, 3, 4])
-    assertEqual "should be Just 'z'" (Just ('z'::Char)) (myLast ['x', 'y', 'z'])
-    assertEqual "should be Nothing" (Nothing:: Maybe Int) (myLast [])
+  assertEqual "should be Just 4" (Just (4 :: Int)) (myLast [1, 2, 3, 4])
+  assertEqual "should be Just 'z'" (Just ('z' :: Char)) (myLast ['x', 'y', 'z'])
+  assertEqual "should be Nothing" (Nothing :: Maybe Int) (myLast [])
+
+-- Problem 2
+testMyButLast :: Test
+testMyButLast = TestCase $ do
+  assertEqual "should be Just 3" (Just (3 :: Int)) (myButLast [1, 2, 3, 4])
+  assertEqual "should be Just 'y'" (Just ('y' :: Char)) (myButLast ['a' .. 'z'])
+  assertEqual "should be Nothing" (Nothing :: Maybe Char) (myButLast ['a'])
+
+-- Problem 3
+testElementAt :: Test
+testElementAt = TestCase $ do
+  assertEqual "should be Just 2" (Just (2 :: Int)) (elementAt [1, 2, 3] 2)
+  assertEqual "should be Just 'e'" (Just ('e' :: Char)) (elementAt "haskell" 5)
+  assertEqual "should be Nothing" (Nothing :: Maybe Int) (elementAt [1, 2] 3)
+
+-- Problem 4
+testMyLength :: Test
+testMyLength = TestCase $ do
+  assertEqual "should be 3" 3 (myLength ([123, 456, 789] :: [Int]))
+  assertEqual "should be 13" 13 (myLength "Hello, world!")
+
+-- Problem 5
+testMyReverse :: Test
+testMyReverse = TestCase $ do
+  assertEqual "should be reverse the string" "!amanap ,lanac a ,nalp a ,nam A" (myReverse "A man, a plan, a canal, panama!")
+  assertEqual "should be reverse the list" [4, 3, 2, 1] (myReverse ([1, 2, 3, 4] :: [Int]))
+
+-- Problem 6
+testIsPalindrome :: Test
+testIsPalindrome = TestCase $ do
+  assertEqual "should be False" False (isPalindrome ([1, 2, 3] :: [Int]))
+  assertEqual "should be True" True (isPalindrome "madamimadam")
+  assertEqual "should be True" True (isPalindrome ([1, 2, 4, 8, 16, 8, 4, 2, 1] :: [Int]))
+
+-- Problem 7
+testFlatten :: Test
+testFlatten = TestCase $ do
+  assertEqual "should be [5]" ([5] :: [Int]) (flatten $ Elem 5)
+  assertEqual "should be [1,2,3,4,5]" ([1, 2, 3, 4, 5] :: [Int]) (flatten $ List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+  assertEqual "should be []" ([] :: [Int]) (flatten $ List [])
+
+-- Problem 8
+testCompress :: Test
+testCompress = TestCase $ do
+  assertEqual "should be abcade" "abcade" (compress "aaaabccaadeeee")
+
+-- Problem 9
+testPack :: Test
+testPack = TestCase $ do
+  assertEqual "should be pack" (["aaaa", "b", "cc", "aa", "d", "eeee"] :: [String]) (pack "aaaabccaadeeee")
+
+-- Problem 10
+testEncode :: Test
+testEncode = TestCase $ do
+  assertEqual "should be encode" [(4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e')] (encode "aaaabccaadeeee")
+
+-- Problem 11
+testEncodeModified :: Test
+testEncodeModified = TestCase $ do
+  assertEqual "should be encode" [Multiple 4 'a', Single 'b', Multiple 2 'c', Multiple 2 'a', Single 'd', Multiple 4 'e'] (encodeModified "aaaabccaadeeee")
+
+-- Problem 12
+testDecodeModified :: Test
+testDecodeModified = TestCase $ do
+  assertEqual "should be decode" "aaaabccaadeeee" (decodeModified [Multiple 4 'a', Single 'b', Multiple 2 'c', Multiple 2 'a', Single 'd', Multiple 4 'e'])
+
+-- Problem 13
+testEncodeDirect :: Test
+testEncodeDirect = TestCase $ do
+  assertEqual "should be encode direct" [Multiple 4 'a', Single 'b', Multiple 2 'c', Multiple 2 'a', Single 'd', Multiple 4 'e'] (encodeDirect "aaaabccaadeeee")
+
+-- Problem 14
+testDupli :: Test
+testDupli = TestCase $ do
+  assertEqual "should be duplicate" ([1, 1, 2, 2, 3, 3] :: [Int]) (dupli [1, 2, 3])
+
+-- Problem 15
+testRepli :: Test
+testRepli = TestCase $ do
+  assertEqual "should be replicate" "aaabbbccc" (repli "abc" 3)
 
 -- Group tests
 tests :: Test
-tests = TestList [TestLabel "Last element of a list" testMyLast]
-
+tests =
+  TestList
+    [ TestLabel "Last element of a list" testMyLast,
+      TestLabel "Penultimate element of a list" testMyButLast,
+      TestLabel "Find element of a list at a given position" testElementAt,
+      TestLabel "Length of a list" testMyLength,
+      TestLabel "Reverse a list" testMyReverse,
+      TestLabel "Palindromes" testIsPalindrome,
+      TestLabel "Flatten a nested list structure" testFlatten,
+      TestLabel "Eliminate duplicate elements in a list" testCompress,
+      TestLabel "Pack duplicates in a list" testPack,
+      TestLabel "Run-length encoding of a list" testEncode,
+      TestLabel "Modified run-length encoding" testEncodeModified,
+      TestLabel "Decode a run-length encoded list" testDecodeModified,
+      TestLabel "Run-length encoding of a list; direct solution" testEncodeDirect,
+      TestLabel "Duplicate elements in a list" testDupli,
+      TestLabel "Replicate elements of a list" testRepli
+    ]
 
 main :: IO ()
 main = do
-    counts' <- runTestTT tests
-    if errors counts' + failures counts' == 0
-        then putStrLn "All tests passed! ✅"
-        else putStrLn "Some tests failed! ❌"
+  counts' <- runTestTT tests
+  if errors counts' + failures counts' == 0
+    then putStrLn "All tests passed! ✅"
+    else putStrLn "Some tests failed! ❌"
